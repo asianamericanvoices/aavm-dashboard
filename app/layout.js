35bb318d9dcd165fa0cfe8b9fc6338a6ab9f63b7
asianamericanvoices <newsroom@asianamericanvoices.us>
2025-08-07 22:48:48 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <head>
+        <script src="https://cdn.tailwindcss.com"></script>
+      </head>
+      <body className="bg-gray-50">{children}</body>
     </html>
   )
 }
